130da21236f1a9dbb8be3655c8104bb9587e7de6
AndersonPaulaSilva <48701676+AndersonPaulaSilva@users.noreply.github.com>
2019-09-14 17:12:06 -0300

    Update JpaTesteApplication.java

--- src/main/java/com/anderson/JpaTesteApplication.java
+++ src/main/java/com/anderson/JpaTesteApplication.java
@@ -7,36 +7,46 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class JpaTesteApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriasRepository categoriasRepository;
+    @Autowired
+    private CategoriasRepository categoriasRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaTesteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTesteApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
+    @Override
+    public void run(String... strings) throws Exception {
 
-		// Metodos a serem executados:
+        // Metodos a serem executados:
 
-		categoriaSalvar();
+        buscarPorId();
 
 
+    }
 
-	}
+    // Implementacoes
 
-	// Implementacoes
+    private void buscarPorId() {
+        Optional<Categoria> optional = categoriasRepository.findById(1);
+        if (optional.isPresent())
+            System.out.println(optional.get());
+        else
+            System.out.println("CATEGORIA NÃO ENCONTRADA");
+    }
 
-	private void categoriaSalvar(){
-		Categoria categoria = new Categoria();
-		categoria.setNome("FINANCEIRO");
-		categoria.setDescricao("Trabalhos de contabilidade");
 
-		categoriasRepository.save(categoria);
-		System.out.println(categoria);
-	}
+    private void categoriaSalvar() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("FINANCEIRO");
+        categoria.setDescricao("Trabalhos de contabilidade");
+
+        categoriasRepository.save(categoria);
+        System.out.println(categoria);
+    }
 
 }
