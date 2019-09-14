6bd0e90c5ea3219b818f2314e9f08ebb8191f558
AndersonPaulaSilva <48701676+AndersonPaulaSilva@users.noreply.github.com>
2019-09-14 14:51:48 -0300

    Update JpaTesteApplication.java

--- src/main/java/com/anderson/JpaTesteApplication.java
+++ src/main/java/com/anderson/JpaTesteApplication.java
@@ -1,13 +1,21 @@
 package com.anderson;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaTesteApplication {
+public class JpaTesteApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaTesteApplication.class, args);
 	}
 
+	@Override
+	public void run(String... strings) throws Exception {
+
+		// Metodos a serem executados:
+
+		System.out.println("PRIMEIRO TESTE EXECUTADO...");
+	}
 }
