dcbab13a8c2afcaff1ff7ca162667501e3a147d7
AndersonPaulaSilva <48701676+AndersonPaulaSilva@users.noreply.github.com>
2019-09-14 16:37:11 -0300

    Teste repositório

--- src/main/java/com/anderson/JpaTesteApplication.java
+++ src/main/java/com/anderson/JpaTesteApplication.java
@@ -1,11 +1,16 @@
 package com.anderson;
 
+import com.anderson.repository.CategoriasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaTesteApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriasRepository categoriasRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaTesteApplication.class, args);
@@ -15,6 +20,8 @@ public class JpaTesteApplication implements CommandLineRunner {
 	public void run(String... strings) throws Exception {
 
 		// Metodos a serem executados:
+
+		System.out.println(categoriasRepository);
 
 
 	}
---
+++ src/main/java/com/anderson/repository/CategoriasRepository.java
@@ -0,0 +1,10 @@
+package com.anderson.repository;
+
+import com.anderson.model.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Anderson on 14/09/2019.
+ */
+public interface CategoriasRepository extends CrudRepository<Categoria,Integer> {
+}
